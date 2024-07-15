be6fa31ebb659f1ea5160eaee4d0ab823ab9c967
Rexjurgoran <hensslermarvin@gmail.com>
2024-07-15 22:19:46 +0200

    (feat): Start implementation using ebiten

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"github.com/hajimehoshi/ebiten"
+)
+
+type Game struct{}
+
+func (g *Game) Update(screen *ebiten.Image) error {
+	// Update the logical state
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Render the screen
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	// Return the game logical screen size.
+	// The screen is automatically scaled.
+	return 320, 240
+}
 
 func main() {
-	fmt.Println("hello world")
+	ebiten.SetWindowSize(640, 400)
+	ebiten.SetWindowTitle("title")
+	game := &Game{}
+	if err := ebiten.RunGame(game); err != nil {
+		panic(err)
+	}
 }
